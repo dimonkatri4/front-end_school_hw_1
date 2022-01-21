d807c504cd9407554b488d1d795af910f75a5760
Dima <katri4dimon@i.ua>
2022-01-21 10:09:30 +0200

    base URL and header config represented in constants

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
+const baseURL = 'https://tiktok33.p.rapidapi.com/';
+const headersConfig = {
+    'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
+    'x-rapidapi-key': 'c1257dc04cmshd888bbb072eb770p1f2b8ajsnbf16d4cd1d66',
+}
+
 const instance = axios.create({
-    baseURL: 'https://tiktok33.p.rapidapi.com/',
+    baseURL,
     withCredentials: true,
-    headers: {
-        'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
-        'x-rapidapi-key': 'c1257dc04cmshd888bbb072eb770p1f2b8ajsnbf16d4cd1d66',
-    },
+    headers: headersConfig,
 });
 export const userAPI = {
     getUserFeed(id = 'dave.xp') {
