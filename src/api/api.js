8218e723087e90706607d64edf83cab7f7d6317b
Dima <katri4dimon@i.ua>
2022-01-21 11:52:35 +0200

    changed the transfer of props from TrendingFeed to Post and applied the prettier the project

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const baseURL = 'https://tiktok33.p.rapidapi.com/';
 const headersConfig = {
     'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
     'x-rapidapi-key': 'c1257dc04cmshd888bbb072eb770p1f2b8ajsnbf16d4cd1d66',
-}
+};
 
 const instance = axios.create({
     baseURL,
--- src/components/MainContent/TrendingFeed/TrendingFeed.js
+++ src/components/MainContent/TrendingFeed/TrendingFeed.js
@@ -20,21 +20,7 @@ const TrendingFeed = function ({ trending, error }) {
     return (
         <div className={style.trendingPage}>
             {trending.map((t) => (
-                <Post
-                    key={t.id}
-                    avatar={t.authorMeta.avatar}
-                    authorName={t.authorMeta.name}
-                    nickName={t.authorMeta.nickName}
-                    postText={t.text}
-                    musicName={t.musicMeta.musicName}
-                    musicAuthor={t.musicMeta.musicAuthor}
-                    videoUrl={t.videoUrl}
-                    userId={t.authorMeta.id}
-                    cover={t.covers.default}
-                    shareCount={t.shareCount}
-                    commentCount={t.commentCount}
-                    diggCount={t.diggCount}
-                />
+                <Post post={t} key={t.id} />
             ))}
         </div>
     );
