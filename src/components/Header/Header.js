08287df9032aaef2222a3b25450f15addb7bc23e
Dima <katri4dimon@i.ua>
2022-02-08 22:47:48 +0200

    import tiktuk-component-library

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {Logo} from "@dimonkatri4/tiktuk-component-library/";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { NavLink } from 'react-router-dom';
 import style from './header.module.css';
-import Logo from './Logo';
+import logo from '../../assets/logo.png';
 
 const Header = function () {
     return (
         <header className="headerPage">
-            <Logo />
+            <Logo logo={logo} />
             <div className={style.headerButtons}>
                 <NavLink to="/profile">
                     <FontAwesomeIcon icon={faHome} />
--- src/pages/Profile/ProfileHeader/ProfileHeader.js
+++ src/pages/Profile/ProfileHeader/ProfileHeader.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import {UserAvatar} from "@dimonkatri4/tiktuk-component-library";
 import PropTypes from 'prop-types';
 import style from '../profile.module.css';
 import UserInfo from './UserInfo';
-import UserAvatar from './UserAvatar';
 
 const ProfileHeader = function ({ profile }) {
     return (
